cbf1256a7502c148bda6de9e2a422874b36ae9a2
Catherine Kwak <khopsickle@users.noreply.github.com>
2017-02-03 08:45:41 -0700

    Setup setup setup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
 //= require angular/angular
 //= require lodash/lodash
 //= require restangular/dist/restangular
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+p 'creating a@a user'
+
+User.create(
+  email: 'a@a.com',
+  password: 'password',
+  password_confirmation: 'password'
+)
